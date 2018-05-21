c32675ddff9b834095e7a42ab66cdfaab78e9533
KukelEugheny <zeeny123@mail.ru>
2018-05-21 22:35:12 +0300

    Task5-7

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const index = (function() {
+create: function() {
+dom.dom.clearMain();
+dom.createAddButton();
+dom.createFilter();
+dom.create3PhotoPost();
+dom.create3PhotoPost();
+dom.create3PhotoPost();
+dom.createShowButton();
+}
+})();
